0ee6aacff7867d118c467d8764339e769511ccb8
Anik Mohammad <anik7303@gmail.com>
2023-10-27 16:10:30 +0600

    group tests together

--- script.test.js
+++ script.test.js
@@ -7,25 +7,27 @@ const mockDatabase = [
   "dogpictures.com",
 ];
 
-test("a silly test", () => {
-  expect("hello").toBe("hello");
-});
+describe("googleSearch", () => {
+  test("a silly test", () => {
+    expect("hello").toBe("hello");
+  });
 
-test("searching google", () => {
-  expect.assertions();
-  expect(googleSearch("testtest", mockDatabase)).toEqual([]);
-  let result = googleSearch("dog", mockDatabase);
-  expect(result.length).toBe(2);
-  expect(result).toEqual(["dogs.com", "dogpictures.com"]);
-});
+  test("searching google", () => {
+    expect.assertions();
+    expect(googleSearch("testtest", mockDatabase)).toEqual([]);
+    let result = googleSearch("dog", mockDatabase);
+    expect(result.length).toBe(2);
+    expect(result).toEqual(["dogs.com", "dogpictures.com"]);
+  });
 
-test("work with undefined and null input", () => {
-  expect.assertions(2);
-  expect(googleSearch(undefined, mockDatabase)).toEqual([]);
-  expect(googleSearch(null, mockDatabase)).toEqual([]);
-});
+  test("work with undefined and null input", () => {
+    expect.assertions(2);
+    expect(googleSearch(undefined, mockDatabase)).toEqual([]);
+    expect(googleSearch(null, mockDatabase)).toEqual([]);
+  });
 
-test("does not return more than 3 matches", () => {
-  expect(googleSearch(".com", mockDatabase).length).toBe(3);
-  expect(googleSearch(".com", mockDatabase).length).toEqual(3);
+  test("does not return more than 3 matches", () => {
+    expect(googleSearch(".com", mockDatabase).length).toBe(3);
+    expect(googleSearch(".com", mockDatabase).length).toEqual(3);
+  });
 });
