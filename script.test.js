89aca3fd747fe067d06ed00a11849bc025bc6330
Anik Mohammad <anik7303@gmail.com>
2023-10-27 16:09:34 +0600

    write more tests

--- script.test.js
+++ script.test.js
@@ -10,3 +10,22 @@ const mockDatabase = [
 test("a silly test", () => {
   expect("hello").toBe("hello");
 });
+
+test("searching google", () => {
+  expect.assertions();
+  expect(googleSearch("testtest", mockDatabase)).toEqual([]);
+  let result = googleSearch("dog", mockDatabase);
+  expect(result.length).toBe(2);
+  expect(result).toEqual(["dogs.com", "dogpictures.com"]);
+});
+
+test("work with undefined and null input", () => {
+  expect.assertions(2);
+  expect(googleSearch(undefined, mockDatabase)).toEqual([]);
+  expect(googleSearch(null, mockDatabase)).toEqual([]);
+});
+
+test("does not return more than 3 matches", () => {
+  expect(googleSearch(".com", mockDatabase).length).toBe(3);
+  expect(googleSearch(".com", mockDatabase).length).toEqual(3);
+});
