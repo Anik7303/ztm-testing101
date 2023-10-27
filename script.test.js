6eeffc7adfe7f060069e4eecaa160efe3c3689db
Anik Mohammad <anik7303@gmail.com>
2023-10-27 16:03:26 +0600

    first test

---
+++ script.test.js
@@ -0,0 +1,12 @@
+const googleSearch = require("./script");
+
+const mockDatabase = [
+  "dogs.com",
+  "cheesepuff.com",
+  "disney.com",
+  "dogpictures.com",
+];
+
+test("a silly test", () => {
+  expect("hello").toBe("hello");
+});
